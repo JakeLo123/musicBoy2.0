1000a64e9364dc32e1cd0534a08ad235c1904be4
jakeloew <jakeloew1@gmail.com>
2019-12-04 18:03:13 -0600

    initial commit

---
+++ sounds/synth.js
@@ -0,0 +1,10 @@
+const Tone = require('tone');
+
+const synth = new Tone.PolySynth({
+  polyphony: 12,
+  volume: 0,
+  detune: 0,
+  voice: Tone.Synth,
+}).toMaster();
+
+module.exports = { synth };
