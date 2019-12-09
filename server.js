2c56401b3a413b2e19ebc4a958a3f054dd961706
jakeloew <jakeloew1@gmail.com>
2019-12-09 17:52:20 -0600

    deploying

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT || 8080;
 const path = require('path');
 
 app.use(express.static('public'));
@@ -9,4 +9,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+app.listen(port, () => console.log(`listening on port ${port}`));
