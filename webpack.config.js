076f425ef031b28e327c0345be780c1e2c33a6dc
jakeloew <jakeloew1@gmail.com>
2019-12-09 17:27:11 -0600

    ready to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   node: {
     fs: 'empty',
   },
-  entry: './main.js',
+  entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
